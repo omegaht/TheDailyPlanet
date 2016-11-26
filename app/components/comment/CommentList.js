0cf33af72538a06f9abf1063a9810c4561f08358
Diego <ddp.trabajo@gmail.com>
2016-11-26 14:45:18 -0400

    grid testing

--- app/components/comment/CommentList.js
+++ app/components/comment/CommentList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // React Boostrap components
-import {Grid} from 'react-bootstrap/lib';
+import {Grid, Row, Col} from 'react-bootstrap/lib';
 // Material-ui components
 import {List, ListItem} from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
@@ -9,20 +9,27 @@ const CommentList = (props) => {
 	if(props.data){
 		return(
 			<Grid>
-				<List>
-					{
-						props.data.map(comment => {
-							return(<ListItem
-                    leftAvatar={<Avatar src="http://static.zerochan.net/Himura.Kenshin.full.551192.jpg" />}
-                    primaryText={comment.user}
-                    secondaryText={
-                        <p>{comment.text}</p>
-                    }
-							/>);
-						})
-					}
-				</List>
+				<Row>
+					<Col xs="{10}">
+					<List>
+						{
+							props.data.map(comment => {
+								return(
+									<ListItem
+											leftAvatar={<Avatar src="http://static.zerochan.net/Himura.Kenshin.full.551192.jpg" />}
+											primaryText={comment.user}
+											secondaryText={
+													<p>{comment.text}</p>
+											}
+									/>
+									);
+							})
+						}
+					</List>
+					</Col>
+				</Row>
 			</Grid>
+			
 		); 
 	}
 	return (<div>"There are no comments to display"</div>);
